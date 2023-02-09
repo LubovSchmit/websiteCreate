b77ba41c53d8bedb6d6166d16c5361bbc8438e87
LiubovSchmit <liubou.schmit@gmail.com>
2023-02-09 15:05:21 +0100

    add gh-pages, refactoring

--- src/pages/main/mainWhatWeDo/services/Services.tsx
+++ src/pages/main/mainWhatWeDo/services/Services.tsx
@@ -8,7 +8,7 @@ export const Services = () => {
     return (
         <div className={style.servicesBlock}>
 
-            <Service serviceName={'Bricolage'} serviceDescription={'Spécialiste de la réparation d’équipement, DOMA vous accompagne pour vos soucis quotidiens'}/>
+            <Service serviceName={'Petit bricolage'} serviceDescription={'Spécialiste de la réparation d’équipement, DOMA vous accompagne pour vos soucis quotidiens'}/>
             <Service serviceName={'Jardin'} serviceDescription={'Vos équipements extérieurs sont aussi notre terrain de jeu'}/>
             <Service serviceName={'Instruments'} serviceDescription={'Les équipements de la maison - on s en occupe aussi!'}/>
 
